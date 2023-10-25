1430e242dce7bb42e63f6ad598804759a7610960
Joe Santhosh <joe.santhosh88@yahoo.com>
2023-10-25 00:56:53 -0400

    dockerized application

--- app.js
+++ app.js
@@ -74,6 +74,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Listening on port 3000");
+server.listen(8080, () => {
+  console.log("Listening on port 8080");
 });
